797d8d734f44d1ccc50847a7de4b7cd723d74ccf
chrisdhodges12 <102702357+chrisdhodges12@users.noreply.github.com>
2023-01-22 08:48:06 -0500

    add layout html template

--- tech-news-java-api/src/main/java/com/technews/controller/CommentController.java
+++ tech-news-java-api/src/main/java/com/technews/controller/CommentController.java
@@ -1,14 +1,5 @@
 package com.technews.controller;
 
-import com.technews.repository.CommentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-public class CommentController {
-
-    package com.technews.controller;
-
 import com.technews.model.Comment;
 import com.technews.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,41 +8,40 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-    @RestController
-    public class CommentController {
+@RestController
+public class CommentController {
 
-        @Autowired
-        CommentRepository repository;
+    @Autowired
+    CommentRepository repository;
 
-        @GetMapping("/api/comments")
-        public List<Comment> getAllComments() {
-            return repository.findAll();
-        }
-
-
-        @GetMapping("/api/comments/{id}")
-        public Comment getComment(@PathVariable int id) {
-            return repository.getById(id);
-        }
+    @GetMapping("/api/comments")
+    public List<Comment> getAllComments() {
+        return repository.findAll();
+    }
 
 
-        @PostMapping("/api/comments")
-        @ResponseStatus(HttpStatus.CREATED)
-        public Comment createComment(@RequestBody Comment comment) {
-            return repository.save(comment);
-        }
+    @GetMapping("/api/comments/{id}")
+    public Comment getComment(@PathVariable int id) {
+        return repository.getById(id);
+    }
 
 
-        @PutMapping("/api/updateComment")
-        public Comment updateComment(@RequestBody Comment comment) {
-            return repository.save(comment);
-        }
+    @PostMapping("/api/comments")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Comment createComment(@RequestBody Comment comment) {
+        return repository.save(comment);
+    }
 
 
-        @DeleteMapping("/api/comments/{id}")
-        @ResponseStatus(HttpStatus.NO_CONTENT)
-        public void deleteComment(@PathVariable int id) {
-            repository.deleteById(id);
-        }
+    @PutMapping("/api/updateComment")
+    public Comment updateComment(@RequestBody Comment comment) {
+        return repository.save(comment);
+    }
+
+
+    @DeleteMapping("/api/comments/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteComment(@PathVariable int id) {
+        repository.deleteById(id);
     }
 }
